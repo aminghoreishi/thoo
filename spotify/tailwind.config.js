2c7e79f858cda7669b08289a5119a0fa53b6730e
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2024-03-09 15:07:48 +0330

    sdf

---
+++ spotify/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pub/*.html"],
+  theme: {
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:"0.8rem",
+        "lg":"1rem",
+        "kj":"1rem"
+      },
+    },  
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
+
